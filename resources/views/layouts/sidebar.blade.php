8f8a85936f8a93e922a6f6642ac57d2a209ebc3c
sofyandenoor <firhanadnan@gmail.com>
2025-10-25 14:24:11 +0700

    perubahan pada view dan js

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,60 @@
+<div data-simplebar class="h-100">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <!-- Left Menu Start -->
+                        <ul class="metismenu list-unstyled" id="side-menu">
+                            <li class="menu-title" key="t-menu">Menu</li>
+
+                            <li>
+                                <a href="javascript: void(0);" class="has-arrow waves-effect">
+                                    <i class="bx bx-home-circle"></i>
+                                    <span key="t-dashboards">Dashboards</span>
+                                </a>
+                                <ul class="sub-menu" aria-expanded="false">
+                                    <li><a href="index.html" key="t-default">Default</a></li>
+                                    <li><a href="dashboard-saas.html" key="t-saas">Saas</a></li>
+                                    <li><a href="dashboard-crypto.html" key="t-crypto">Crypto</a></li>
+                                    <li><a href="dashboard-blog.html" key="t-blog">Blog</a></li>
+                                    <li><a href="dashboard-job.html" key="t-jobs">Jobs</a></li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);" class="waves-effect">
+                                    <span class="badge rounded-pill bg-danger float-end" key="t-hot">Hot</span>
+                                    <i class="bx bx-layout"></i>
+                                    <span key="t-layouts">Layouts</span>
+                                </a>
+                                <ul class="sub-menu" aria-expanded="true">
+                                    <li>
+                                        <a href="javascript: void(0);" class="has-arrow" key="t-vertical">Vertical</a>
+                                        <ul class="sub-menu" aria-expanded="true">
+                                            <li><a href="layouts-light-sidebar.html" key="t-light-sidebar">Light Sidebar</a></li>
+                                            <li><a href="layouts-compact-sidebar.html" key="t-compact-sidebar">Compact Sidebar</a></li>
+                                            <li><a href="layouts-icon-sidebar.html" key="t-icon-sidebar">Icon Sidebar</a></li>
+                                            <li><a href="layouts-boxed.html" key="t-boxed-width">Boxed Width</a></li>
+                                            <li><a href="layouts-preloader.html" key="t-preloader">Preloader</a></li>
+                                            <li><a href="layouts-colored-sidebar.html" key="t-colored-sidebar">Colored Sidebar</a></li>
+                                            <li><a href="layouts-scrollable.html" key="t-scrollable">Scrollable</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li>
+                                        <a href="javascript: void(0);" class="has-arrow" key="t-horizontal">Horizontal</a>
+                                        <ul class="sub-menu" aria-expanded="true">
+                                            <li><a href="layouts-horizontal.html" key="t-horizontal">Horizontal</a></li>
+                                            <li><a href="layouts-hori-topbar-light.html" key="t-topbar-light">Topbar light</a></li>
+                                            <li><a href="layouts-hori-boxed-width.html" key="t-boxed-width">Boxed width</a></li>
+                                            <li><a href="layouts-hori-preloader.html" key="t-preloader">Preloader</a></li>
+                                            <li><a href="layouts-hori-colored-header.html" key="t-colored-topbar">Colored Header</a></li>
+                                            <li><a href="layouts-hori-scrollable.html" key="t-scrollable">Scrollable</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <!-- Sidebar -->
+                </div>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pages.beranda');
+    return view('layouts.master');
 });
 
 Route::get('/contact', function () {
